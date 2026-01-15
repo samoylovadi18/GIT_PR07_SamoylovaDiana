925ac5525310354cfa4b32f2a9d08368596d5fce
samoylovadi18 <samoylovadi18@gmail.com>
2026-01-15 15:39:52 +0300

    Revert "дизайн формы администратора"
    This reverts commit 492539d543533be35fc1b61424cff72d32767d71.

--- GIT_PR07_SamoylovaDiana/AdminForm2.Designer.cs
+++ GIT_PR07_SamoylovaDiana/AdminForm2.Designer.cs
@@ -33,60 +33,24 @@
             this.printDialog1 = new System.Windows.Forms.PrintDialog();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.label1 = new System.Windows.Forms.Label();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.button1 = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // printDialog1
             // 
             this.printDialog1.UseEXDialog = true;
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 26);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(154, 24);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "Администратор";
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Image = global::GIT_PR07_SamoylovaDiana.Properties.Resources.Снимок_экрана_2026_01_15_150634;
-            this.pictureBox1.Location = new System.Drawing.Point(615, 42);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(142, 117);
-            this.pictureBox1.TabIndex = 4;
-            this.pictureBox1.TabStop = false;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(31, 442);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(270, 92);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "получить сообщение";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // AdminForm2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(784, 561);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(6);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
             this.Name = "AdminForm2";
             this.Text = "AdminForm2";
             this.Load += new System.EventHandler(this.AdminForm2_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -94,8 +58,5 @@
         private System.Windows.Forms.PrintDialog printDialog1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.Button button1;
     }
 }
